7ebeb42154392230e253e68e840fb0a42467338a
Raul <raulalfonzo66@gmail.com>
2025-02-09 12:12:06 -0400

    feat (reactive CRUD): initial commit for the reactive version of the CRUD

---
+++ reactive/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Container } from 'react-bootstrap'
+import Card from './components/Card/Card';
+
+
+function App() {
+
+  return (
+      <Container>
+        <Row>
+          <Card />
+          <Card />
+        </Row>
+        <Row className='mt-2'>
+          <Card />
+        </Row>
+      </Container>
+  )
+}
+
+export default App
---
+++ reactive/src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import './Card.css'
+import { Col } from 'react-bootstrap'
+
+const Card = () => {
+  return (
+    <Col>
+      <div className="section">
+        Card
+      </div>
+    </Col> 
+  )
+}
+
+export default Card
