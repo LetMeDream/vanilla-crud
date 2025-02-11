e00c5fba15d9419c60f85811593ef17dd2811c98
Raul <raulalfonzo66@gmail.com>
2025-02-11 17:40:21 -0400

    fix(EditProduct): add FormProvider on the EditProduct component to avoid error

--- reactive/src/components/Sections/EditProduct.jsx
+++ reactive/src/components/Sections/EditProduct.jsx
@@ -4,6 +4,7 @@ import SelectInput from "../Inputs/SelectInput/SelectInput"
 import TextInput from "../Inputs/TextInput/TextInput"
 import PropTypes from "prop-types"
 import {Button} from "react-bootstrap"
+import { useForm, FormProvider } from "react-hook-form"
 
 const EditProduct = ({
   xs,
@@ -12,39 +13,43 @@ const EditProduct = ({
   lg, 
   classnames
 }) => {
+  const methods = useForm()
+
   return (
     <Col  xs={xs} sm={sm} md={md} lg={lg}>
-      <Card classnames={classnames}>  
-        <h1 className="section-title">
-          Editar
-        </h1>
-        {/* Producto start */}
-        <SelectInput 
-          title={'Producto'}
-          itemToSelect={'producto'}
-        />
-        {/* Producto start */}
-        {/* Atributo start */}
-        <SelectInput 
-          title={'Atributo'}
-          itemToSelect={'atributo'}
-        />  
-        {/* Atributo end */}
-        {/* new value start */}
-        <TextInput
-          title='Nuevo valor'
-          type='text'
-          id='product-new-value'
-          placeholder={'Nuevo valor'}
-        />
-        {/* new value end */}
-        {/* Nombre end */}
-        <div className="flex">
-          <Button className="btn-send" variant="outline-light" size="sm">
+      <FormProvider {...methods}>
+        <Card classnames={classnames}>  
+          <h1 className="section-title">
             Editar
-          </Button>
-        </div>
-      </Card>
+          </h1>
+          {/* Producto start */}
+          <SelectInput 
+            title={'Producto'}
+            itemToSelect={'producto'}
+          />
+          {/* Producto start */}
+          {/* Atributo start */}
+          <SelectInput 
+            title={'Atributo'}
+            itemToSelect={'atributo'}
+          />  
+          {/* Atributo end */}
+          {/* new value start */}
+          <TextInput
+            title='Nuevo valor'
+            type='text'
+            id='product-new-value'
+            placeholder={'Nuevo valor'}
+          />
+          {/* new value end */}
+          {/* Nombre end */}
+          <div className="flex">
+            <Button className="btn-send" variant="outline-light" size="sm">
+              Editar
+            </Button>
+          </div>
+        </Card>
+      </FormProvider>
     </Col>
     )
 }
