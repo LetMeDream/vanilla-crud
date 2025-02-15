3c6bf35d357705f509f22cee80ef5607a64cc61d
Raul <raulalfonzo66@gmail.com>
2025-02-15 12:36:38 -0400

    feat(DeleteProduct): crear el Schema de validación para el formulario de Eliminación de producto

--- reactive/src/constants/Schemas.js
+++ reactive/src/constants/Schemas.js
@@ -2,10 +2,15 @@ import * as yup from 'yup'
 
 /* Archivo para definir los Schemas de los formularios a utilizar. */
 
-/* Schema del formulario de AÑADIR PRODUCTO */
-export const addProductForm = yup.object({
+/* Schema para el formulario de AÑADIR PRODUCTO */
+export const addProductFormSchema = yup.object({
   'product-name': yup.string().required('Este campo es obligatorio'),
   'product-value': yup.number().typeError('Éste valor debe ser un número').required('Este campo es obligatorio'),
   'product-existance': yup.number().typeError('Éste valor debe ser un número').required('Este campo es obligatorio'),
   'product-url-image': yup.string().url('Debe ser una URL válida').required('Este campo es obligatorio')
+})
+
+/* Schema para el formulatio de ELIMINAR PRODUCTO */
+export const deleteProductFormSchema = yup.object({
+  'product-name': yup.string().required('Este campo es obligatorio'),
 })
