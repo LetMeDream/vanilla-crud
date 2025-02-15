e4f2c9c31e04a7bee500d62b68d9402e9393c2ac
Raul <raulalfonzo66@gmail.com>
2025-02-15 12:42:14 -0400

    feat(SelectInput): crear custom hook 'useSelectInput' para manejar la lógica del componente SelectInput

--- reactive/src/hooks/useInputs.js
+++ reactive/src/hooks/useInputs.js
@@ -7,7 +7,7 @@ export const useTextInput = (
   const { formState: {errors} } = useFormContext()
   const [error, setError] = useState(null)          // Error controlará mostrar o no el mensaje de error.
   const [isError, setIsError] = useState(null)      // Mientras que 'isError' controlará las clases CSS para el borde rojo de los Inputs, al momento del error.
-  const value = useWatch({name: id})
+  const value = useWatch({name: id})                // useWatch nos permite observar en tiempo real los cambios al field que le pasemos.
 
   /* useEffect para marcar o desmarcar el error en el input */
   useEffect(() => {
@@ -35,4 +35,38 @@ export const useTextInput = (
     error,
     isError
   }
+}
+
+export const useSelectInput = (id) => {
+  const { formState: {errors} } = useFormContext()
+  const [error, setError] = useState(null)          // Error controlará mostrar o no el mensaje de error.
+  const [isError, setIsError] = useState(null)      // Mientras que 'isError' controlará las clases CSS para el borde rojo de los Inputs, al momento del error.
+  const value = useWatch({name: id})                // useWatch nos permite observar en tiempo real los cambios al field que le pasemos.
+  
+    /* useEffect para marcar o desmarcar el error en el input */
+    useEffect(() => {
+      if (errors?.[id]){
+        setError(errors?.[id])
+        setIsError(true)
+      } else {
+        setError(null)
+      }
+    }, [errors, id])
+  
+    /* useEffect para quitar el color rojo de los errores en los Fields, luego de pasado un tiempo */
+    useEffect(() => {
+      setTimeout(() => {
+        if(errors) setIsError(false)
+      }, 2500);
+    }, [errors])
+  
+    /* useEffect para quitar mensaje de error, si el usuario modifica el input erróneo en cuestión */
+    useEffect(() => {
+      if(value) setError(false)
+    }, [value])
+  
+    return {
+      error,
+      isError
+    }
 }
