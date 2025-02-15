c8d9b4cf7f1656ac4b4f0ff19ee266ff014a304f
Raul <raulalfonzo66@gmail.com>
2025-02-15 12:37:45 -0400

    feat(DeleteProduct): continuar y finalizar lógica en el componente DeleteProduct. En éste commit se: - Crea el formulario en el componente y se añade al FormProvider. - importa función useForm, para instanciar formulario, y se le pasa el   Schema que lo validará. - Crea la función onClick, para abrir el modal de confirmación. - Crea la función 'deleteProduct' para eliminar productos.

--- reactive/src/components/Sections/DeleteProduct.jsx
+++ reactive/src/components/Sections/DeleteProduct.jsx
@@ -2,7 +2,14 @@ import Card from "../FormCard/FormCard"
 import { Col } from "react-bootstrap"
 import PropTypes from "prop-types"
 import SelectInput from "../Inputs/SelectInput/SelectInput"
-import {Button} from "react-bootstrap"
+import CustomButton from "../Shared/CustomButton"
+import { useForm, FormProvider } from "react-hook-form"
+import { deleteProductFormSchema } from "../../constants/Schemas"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { errorToast } from "../../helpers/toasts"
+import { BasicModal } from "../Shared/Modal/Modal"
+import { useModal } from "../../hooks/useModal"
+import { useState } from "react"
 
 const DeleteProduct = ({
   xs,
@@ -10,29 +17,71 @@ const DeleteProduct = ({
   md, 
   lg, 
   classnames = '',
-  products
+  products,
+  setProducts
 }) => {
+  const methods = useForm({
+    resolver: yupResolver(deleteProductFormSchema)
+  })
+  const { trigger, handleSubmit, register, getValues } = methods
+  const { isModalOpen, closeModal, openModal } = useModal()
+  const [productToDelete, setProductToDelete] = useState(null)
+
+  const onClick = async () => {
+    if (await trigger()){
+      let value = getValues('product-name')
+      setProductToDelete(products?.filter(product => product?.id === value)[0])
+      openModal()
+    } else {
+      errorToast({
+        title: 'Ups!',
+        content: 'Revise los errores del formulario'
+      })
+    }
+  }
+
+  const deleteProduct = () => {
+    setProducts(prevProducts => prevProducts.filter(product => product?.id !== productToDelete?.id))
+  } 
+
   return (
     <Col xs={xs} sm={sm} md={md} lg={lg}
       className={classnames}
-    >
-      <Card>
-        <h1 className="section-title">
-          Eliminar
-        </h1>
-        {/* Atributo start */}
-        <SelectInput 
-          title={'Producto'}
-          itemToSelect={'producto'}
-          products={products}
-        />  
-        {/* Nombre end */}
-        <div className="flex">
-          <Button className="btn-send" variant="outline-light" size="sm">
-            Eliminar
-          </Button>
-        </div>
-      </Card>
+    > 
+      <FormProvider 
+        {...methods}
+      >
+        <form onSubmit={handleSubmit(onClick)}>
+          <Card>
+            <h1 className="section-title">
+              Eliminar
+            </h1>
+            {/* Atributo start */}
+            <SelectInput 
+              title={'Producto'}
+              itemToSelect={'producto'}
+              products={products}
+              register={register}
+              id='product-name'
+            />  
+            {/* Nombre end */}
+            <div className="flex">
+              <CustomButton
+                text={'Eliminar'}
+                buttonType={'submit'}
+                classnames={'mt-2'}
+                onClick={onClick}
+              />
+            </div>
+          </Card>
+        </form>
+        <BasicModal
+          show={isModalOpen}
+          handleClose={closeModal}
+          productToDelete={productToDelete}
+          action={deleteProduct}
+        />
+      </FormProvider>
     </Col>
   )
 }
@@ -43,7 +92,8 @@ DeleteProduct.propTypes = {
   md: PropTypes.number,
   lg: PropTypes.number,
   classnames: PropTypes.string,
-  products: PropTypes.array
+  products: PropTypes.array,
+  setProducts: PropTypes.func
 }
 
 export default DeleteProduct
